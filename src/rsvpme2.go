56ad016a644a72e2d836772f76d7c0d75c6573e5
hankhank <andrew@sideramota.com>
2013-08-18 06:52:12 +1000

    More

--- src/rsvpme2.go
+++ src/rsvpme2.go
@@ -11,101 +11,126 @@ import (
     "io"
     "net/http"
     "net/url"
-    "regexp"
+    "time"
+    "container/list"
 )
-
-
-//func (p *Page) save() error {
-//    filename := "data/" + p.Title + ".txt"
-//    return ioutil.WriteFile(filename, p.Body, 0600)
-//}
-//
-//func loadPage(title string) (*Page, error) {
-//    filename := "data/" + title + ".txt"
-//    body, err := ioutil.ReadFile(filename)
-//    if err != nil {
-//        return nil, err
-//    }
-//    return &Page{Title: title, Body: body}, nil
-//}
-
-var linkMatcher = regexp.MustCompile(`\[[a-zA-Z0-9]+\]`)
-
-func replaceLinks(match []byte) []byte {
-  last := len(match) - 1
-  page := match[1:last]
-
-  var w bytes.Buffer
-  err := templates.ExecuteTemplate(&w, "link.html", string(page))
-  if err != nil {
-    return match
-  }
-  return []byte(w.String())
-}
-
-func requestAuthHandler(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "https://secure.meetup.com/oauth2/authorize?client_id=1q436aibkm3lpb5daoioul87tk&response_type=code&redirect_uri=http://127.0.0.1:8080/authed", http.StatusFound)
-}
-
-func authHandler(w http.ResponseWriter, r *http.Request) {
-    accessTokenUrl := "https://secure.meetup.com/oauth2/access"
-    clientId       := "1q436aibkm3lpb5daoioul87tk"
-    clientSecret   := "pj1gksajgksruc39gjh81udebt"
-    grantType      := "authorization_code"
-    redirectUri    := "http://127.0.0.1:8080/authed"
-    code           := r.URL.Query().Get("code")
-    v := url.Values{}
-    v.Set("client_id", clientId)
-    v.Set("client_secret", clientSecret)
-    v.Set("grant_type",grantType)
-    v.Set("redirect_uri", redirectUri)
-    v.Set("code", code)
-    resp, err:=http.PostForm(accessTokenUrl, v)
-    fmt.Println(resp)
-    fmt.Println(err)
-}
-
-var templates = template.Must(template.ParseFiles("tmpl/home.html", "tmpl/wegoit.html"))
-
-//func handleFunc (path string, fn func(http.ResponseWriter, *http.Request, string)) {
-//  lenPath := len(path)
-//  handler := func(w http.ResponseWriter, r *http.Request) {
-//    title := r.URL.Path[lenPath:]
-//    if !titleValidator.MatchString(title) {
-//      http.NotFound(w, r)
-//      return
-//    }
-//    fn(w, r, title)
-//    }
-//
-//  http.HandleFunc(path, handler)
-//}
 
 type HomePage struct {
     RequestUri string
+    RefererUri string
+    ClientId string
 }
 
-func addRootHandler(uri string) {
+type WeGotItPage struct {
+    EventInfo string
+}
+
+type RsvpRequest struct {
+    RequestTime     time.Time
+    TokenExpireTime time.Time
+    AcccessToken    string
+    RefreshToken    string
+    EventUrl        string
+    RsvpTime       time.Time
+}
+
+var templates = template.Must(template.ParseFiles("tmpl/home.html", "tmpl/wegotit.html"))
+
+
+func addRootHandler(uri string, refererUri string, clientId string) {
     homePage := new(bytes.Buffer)
-    err:=templates.ExecuteTemplate(homePage, "home.html", HomePage{RequestUri: uri})
-    fmt.Println(err)
+    templates.ExecuteTemplate(homePage, "home.html", HomePage{RequestUri: uri, RefererUri: refererUri, ClientId: clientId})
     handler := func(w http.ResponseWriter, r *http.Request) {
-        io.Copy(w, homePage)
+        io.Copy(w, bytes.NewBuffer(homePage.Bytes()))
     }
     fmt.Println(homePage)
     http.HandleFunc("/", handler)
 }
 
+func addAuthHandler(ch chan RsvpRequest, accessUrl string, refererUri string, clientId string, clientSecret string) {
+    
+    handler := func(w http.ResponseWriter, r *http.Request) {
+        code := r.URL.Query().Get("code")
+        v := url.Values{}
+        v.Set("client_id", clientId)
+        v.Set("client_secret", clientSecret)
+        v.Set("grant_type", "authorization_code")
+        v.Set("redirect_uri", refererUri)
+        v.Set("code", code)
+        resp, err := http.PostForm(accessUrl, v)
+        if (err == nil && resp.StatusCode == http.StatusOK) {
+            buf := new(bytes.Buffer)
+            defer resp.Body.Close()
+            io.Copy(buf, resp.Body)
+            ch <- RsvpRequest{EventUrl: r.URL.Query().Get("state")}
+            http.Redirect(w, r, "/wegotit", http.StatusFound)
+        } else {
+            http.Redirect(w, r, "/fark", http.StatusFound)
+        }
+    }
+    http.HandleFunc("/authed/", handler)
+}
+
+func weGotIt(w http.ResponseWriter, r *http.Request) {
+    templates.ExecuteTemplate(w, "wegotit.html", WeGotItPage{EventInfo: ""})
+}
+
+func startWait(waitch chan RsvpRequest, rsvpList *list.List) {
+
+    mintime := time.Now()
+    nextEvent := rsvpList.Front()
+
+    for e := rsvpList.Front(); e != nil; e = e.Next() {
+        rsvp := e.Value.(RsvpRequest)
+        if mintime.Before(rsvp.RsvpTime) {
+            mintime = rsvp.RsvpTime
+            nextEvent = e
+        }
+    }
+    
+    ch := time.After(mintime.Sub(time.Now()))
+    select {
+        case <- ch:
+    }
+    waitch <- nextEvent.Value.(RsvpRequest)
+    fmt.Println(nextEvent)
+    rsvpList.Remove(nextEvent)
+}
+
+func rsvpToEvent(event RsvpRequest) {
+}
+
+func rsvpTask(ch chan RsvpRequest) {
+       
+    // Read prev rsvp requests if we crashed
+    waitch := make(chan RsvpRequest)
+    rsvpList := list.New()
+    for {
+        select {
+            case rsvp := <-ch: {
+                rsvpList.PushBack(rsvp)
+            }
+            case event := <- waitch: {
+                rsvpToEvent(event)
+            }
+        }
+        fmt.Println("asd")
+        go startWait(waitch, rsvpList)
+    }
+}
+
 func main() {
     fmt.Println("starting")
-    clientId       := "1q436aibkm3lpb5daoioul87tk"
-    //grantType      := "authorization_code"
-    redirectUri    := "http://127.0.0.1:8080/authed"
-    requestUri := fmt.Sprintf("https://secure.meetup.com/oauth2/authorize?client_id=%v&response_type=code&redirect_uri=%v", clientId, redirectUri)
-    //homePage = templates.ExecuteTemplate(w, "home.html",  HomePage{requestUri: requestUri})
-    fmt.Println(requestUri)
-    addRootHandler(requestUri)
-    http.HandleFunc("/add", requestAuthHandler)
-    http.HandleFunc("/authed", authHandler)
+    clientId      := "1q436aibkm3lpb5daoioul87tk"
+    redirectUri   := "http://127.0.0.1:8080/authed/"
+    authorizeUrl  := "https://secure.meetup.com/oauth2/authorize"
+    accessUrl   := "https://secure.meetup.com/oauth2/access"
+    
+    ch := make(chan RsvpRequest)
+    go rsvpTask(ch)
+
+    addRootHandler(authorizeUrl, redirectUri, clientId)
+    addAuthHandler(ch, accessUrl, redirectUri, clientId, clientSecret)
+    http.HandleFunc("/wegotit", weGotIt)
     http.ListenAndServe(":8080", nil)
 }
